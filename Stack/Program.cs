057f11a5bf8f4aadd27a5a31c05c71e40d7dbabb
ch1llay <ch1llay@yandex.ru>
2021-10-21 19:56:16 +0300

    Добавьте файлы проекта.

---
+++ Stack/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program
+    {
+        class Element
+        {
+            public Element Next;
+            object value;
+            public object Value
+            {
+                get
+                {
+                    return value;
+                }
+            }
+            public Element(object value)
+            {
+                this.value = value;
+            }
+        }
+        class Stack
+        {
+            Element top = null;
+            int lenght = 0;
+            public void Add(object value)
+            {
+                if (top == null)
+                {
+                    top = new Element(value);
+                }
+                else
+                {
+                    Element newElement = new Element(value); ;
+                    newElement.Next = top;
+                    top = newElement;
+                }
+                lenght++;
+            }
+            public void Show()
+            {
+                Element tempTop = top;
+                for(int i = 0; i < lenght; i++)
+                {
+                    Console.WriteLine(tempTop.Value);
+                    tempTop = tempTop.Next;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Add(1);
+            stack.Add(2);
+            stack.Add(3);
+            stack.Show();
+            
+        }
+    }
+}
